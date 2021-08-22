464bdb732383e76dcd3b57ab115876d2cfd7d8d5
Acrdiaz <acrdiaz@msn.com>
2021-08-21 22:06:27 -0400

    Add class BaseRun

---
+++ Coding/Base/BaseRun.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding.Base
+{
+    public abstract class BaseRun
+    {
+        public abstract void Run();
+    }
+}
